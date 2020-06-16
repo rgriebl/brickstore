ab0e32336b9fff653aef4db5f506362fa7af8b03
Robert Griebl <robert@griebl.org>
2020-06-16 20:08:42 +0200

    Re-fix lzmadec on MSVC 2017

--- src/lzma/lzmadec.h
+++ src/lzma/lzmadec.h
@@ -63,6 +63,8 @@ extern "C" {
     typedef          __int32  int32_t;
     typedef          __int16  int16_t;
     typedef          __int8   int8_t;
+#else
+#include <inttypes.h>
 #endif
 #else
 #include <inttypes.h>
