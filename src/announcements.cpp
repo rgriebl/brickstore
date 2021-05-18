3ae61da10ee3eee329ce985075709bb02d94fcd5
Robert Griebl <robert@griebl.org>
2021-05-18 13:04:59 +0200

    Fix build against Qt < 5.14

--- src/announcements.cpp
+++ src/announcements.cpp
@@ -242,7 +242,7 @@ AnnouncementsDialog::AnnouncementsDialog(const QString &markdown, QWidget *paren
     m_browser->setReadOnly(true);
     m_browser->setFrameStyle(QFrame::NoFrame);
 #if QT_VERSION < QT_VERSION_CHECK(5, 14, 0)
-    browser->setHtml(markdown);
+    m_browser->setPlainText(markdown);
 #else
     m_browser->setMarkdown(markdown % "\n\n___\n"_l1);
 #endif
