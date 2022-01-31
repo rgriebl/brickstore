rgriebl/brickstore

commit dbd57ebed0c2a4c26584434c93b93fa10cbb11b9

Fixed the instantiation order of the dock widgets

src/desktop/mainwindow.cpp
@@ -152,33 +152,6 @@ MainWindow::MainWindow(QWidget *parent)
 
     // vvv increase DockStateVersion if you change the dock/toolbar setup
 
-    setCorner(Qt::BottomLeftCorner, Qt::LeftDockWidgetArea);
-    setCorner(Qt::BottomRightCorner, Qt::RightDockWidgetArea);
-    setCorner(Qt::TopLeftCorner, Qt::LeftDockWidgetArea);
-    setCorner(Qt::TopRightCorner, Qt::RightDockWidgetArea);
-
-    setDockOptions(AnimatedDocks | AllowNestedDocks | AllowTabbedDocks);
-
-    auto dockInfo = createDock(new TaskInfoWidget, "dock_info");
-    auto dockOpen = createDock(new TaskOpenDocumentsWidget, "dock_opendocuments");
-    auto dockRecent = createDock(new TaskRecentDocumentsWidget, "dock_recentdocuments");
-    auto dockAppearsIn = createDock(new TaskAppearsInWidget, "dock_appearsin");
-    auto dockPriceGuide = createDock(new TaskPriceGuideWidget, "dock_priceguide");
-
-    addDockWidget(Qt::LeftDockWidgetArea, dockInfo);
-    addDockWidget(Qt::LeftDockWidgetArea, dockOpen);
-    addDockWidget(Qt::LeftDockWidgetArea, dockRecent);
-    addDockWidget(Qt::LeftDockWidgetArea, dockAppearsIn);
-    addDockWidget(Qt::LeftDockWidgetArea, dockPriceGuide);
-    tabifyDockWidget(dockRecent, dockOpen);
-    tabifyDockWidget(dockOpen, dockInfo);
-    tabifyDockWidget(dockAppearsIn, dockPriceGuide);
-
-    auto logDock = createDock(m_devConsole, "dock_errorlog");
-    logDock->setAllowedAreas(Qt::BottomDockWidgetArea);
-    logDock->setVisible(false);
-    addDockWidget(Qt::BottomDockWidgetArea, logDock, Qt::Horizontal);
-
     m_toolbar = new QToolBar(this);
     m_toolbar->setObjectName("toolbar"_l1);
     m_toolbar->setMovable(false);
@@ -188,8 +161,6 @@ MainWindow::MainWindow(QWidget *parent)
         if (m_welcomeWidget->isVisible())
             repositionHomeWidget();
     });
-
-    // ^^^ increase DockStateVersion if you change the dock/toolbar setup
 
     auto setIconSizeLambda = [this](Config::IconSize iconSize) {
         static const QMap<Config::IconSize, QStyle::PixelMetric> map = {
@@ -208,6 +179,7 @@ MainWindow::MainWindow(QWidget *parent)
     setupMenuBar();
     setupToolBar();
     addToolBar(m_toolbar);
+    setupDockWidgets();
 
     createCentralWidget();
 
@@ -854,6 +826,38 @@ bool MainWindow::setupToolBar()
         }
     }
     return true;
+}
+
+void MainWindow::setupDockWidgets()
+{
+    // vvv increase DockStateVersion if you change the dock/toolbar setup
+
+    setCorner(Qt::BottomLeftCorner, Qt::LeftDockWidgetArea);
+    setCorner(Qt::BottomRightCorner, Qt::RightDockWidgetArea);
+    setCorner(Qt::TopLeftCorner, Qt::LeftDockWidgetArea);
+    setCorner(Qt::TopRightCorner, Qt::RightDockWidgetArea);
+
+    setDockOptions(AnimatedDocks | AllowNestedDocks | AllowTabbedDocks);
+
+    auto dockInfo = createDock(new TaskInfoWidget, "dock_info");
+    auto dockOpen = createDock(new TaskOpenDocumentsWidget, "dock_opendocuments");
+    auto dockRecent = createDock(new TaskRecentDocumentsWidget, "dock_recentdocuments");
+    auto dockAppearsIn = createDock(new TaskAppearsInWidget, "dock_appearsin");
+    auto dockPriceGuide = createDock(new TaskPriceGuideWidget, "dock_priceguide");
+
+    addDockWidget(Qt::LeftDockWidgetArea, dockInfo);
+    addDockWidget(Qt::LeftDockWidgetArea, dockOpen);
+    addDockWidget(Qt::LeftDockWidgetArea, dockRecent);
+    addDockWidget(Qt::LeftDockWidgetArea, dockAppearsIn);
+    addDockWidget(Qt::LeftDockWidgetArea, dockPriceGuide);
+    tabifyDockWidget(dockRecent, dockOpen);
+    tabifyDockWidget(dockOpen, dockInfo);
+    tabifyDockWidget(dockAppearsIn, dockPriceGuide);
+
+    auto logDock = createDock(m_devConsole, "dock_errorlog");
+    logDock->setAllowedAreas(Qt::BottomDockWidgetArea);
+    logDock->setVisible(false);
+    addDockWidget(Qt::BottomDockWidgetArea, logDock, Qt::Horizontal);
 }
 
 QMenu *MainWindow::setupMenu(const QByteArray &name, const QVector<QByteArray> &a_names)

src/desktop/mainwindow.h
@@ -108,6 +108,7 @@ private:
     QMenu *setupMenu(const QByteArray &, const QVector<QByteArray> &);
     void setupMenuBar();
     bool setupToolBar();
+    void setupDockWidgets();
     QDockWidget *createDock(QWidget *widget, const char *name);
     void forEachViewPane(std::function<bool(ViewPane *)> callback);
     void setActiveViewPane(ViewPane *newActive);

commit ac343eab4917b8865a1cccde7ec1a49cc6eaef88

Fixed the dock menu not showing
This got broke in dbd57ebe

src/desktop/mainwindow.cpp
@@ -560,8 +560,12 @@ void MainWindow::setupMenuBar()
     ActionManager *am = ActionManager::inst();
 
     auto *viewDocksMenu = am->qAction("view_docks")->menu();
-    for (QDockWidget *dock : qAsConst(m_dock_widgets))
-        viewDocksMenu->addAction(dock->toggleViewAction());
+    connect(viewDocksMenu, &QMenu::aboutToShow,
+            this, [this, viewDocksMenu]() {
+        viewDocksMenu->clear();
+        for (QDockWidget *dock : qAsConst(m_dock_widgets))
+            viewDocksMenu->addAction(dock->toggleViewAction());
+    });
 
     setupMenu("document_import", {
                   "document_import_bl_inv",