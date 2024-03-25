a1dab6af76733bd3109636c5b65d9d750d495c2f
Robert Griebl <robert@griebl.org>
2024-03-25 21:40:46 +0100

    Fix online detection on Android

--- src/common/onlinestate.cpp
+++ src/common/onlinestate.cpp
@@ -21,7 +21,7 @@ OnlineState::OnlineState(QObject *parent)
 {
 #if QT_VERSION < QT_VERSION_CHECK(6, 3, 0)
     if (!QNetworkInformation::load(QNetworkInformation::Feature::Reachability)) {
-#elif QT_VERSION >= QT_VERSION_CHECK(6, 4, 0) && defined(Q_OS_LINUX)
+#elif QT_VERSION >= QT_VERSION_CHECK(6, 4, 0) && defined(Q_OS_LINUX) && !defined(Q_OS_ANDROID)
     // the glib one works way better, plus the NM (default) one is blocked inside snaps
     if (!QNetworkInformation::loadBackendByName(u"glib") && !QNetworkInformation::loadDefaultBackend()) {
 #else
