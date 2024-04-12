1131cdfd3452619a4bae2dc1261daec7c139be49
Robert Griebl <robert@griebl.org>
2024-04-12 13:59:14 +0200

    Notify users on macOS who are using the legacy version, but shouldn't

--- src/desktop/desktopapplication.cpp
+++ src/desktop/desktopapplication.cpp
@@ -42,8 +42,10 @@
 #  endif
 #elif defined(Q_OS_MACOS)
 #  include <QtCore/QVersionNumber>
+#  include <QtCore/QSysInfo>
 #  include <QtGui/private/qguiapplication_p.h>
 #  include <QtGui/private/qshortcutmap_p.h>
+#  include "common/systeminfo.h"
 #endif
 
 #include "common/config.h"
@@ -184,6 +186,21 @@ void DesktopApplication::init()
             break;
         default:
             break;
+        }
+    }
+#endif
+
+#if defined(Q_OS_MACOS)
+    if (SystemInfo::inst()->value(u"build.qt.version"_qs).toString() == u"6.4.3") {
+        if (!QSysInfo::productVersion().startsWith(u"10.")) {
+            QString text = tr("You are using the legacy version of BrickStore for old macOS 10 "
+                              "machines, but you are running macOS %1.")
+                               .arg(QSysInfo::productVersion())
+                           + u"<br><br>"
+                           + tr("Please download <a href='https://%1/releases/latest'>the normal "
+                                "macOS version</a> and replace the current installation to avoid problems.")
+                                 .arg(gitHubUrl());
+            QMessageBox::critical(MainWindow::inst(), QCoreApplication::applicationName(), text);
         }
     }
 #endif
