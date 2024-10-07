7e747cc79bcd3a5893fc3022cd01e320696adc01
Robert Griebl <robert@griebl.org>
2024-10-07 22:55:10 +0200

    Linux: fix QThreadStorage crash on exit due to libtbb threads

--- src/main.cpp
+++ src/main.cpp
@@ -15,23 +15,33 @@ using ApplicationType = DesktopApplication;
 using ApplicationType = MobileApplication;
 #endif
 
+#if defined(BS_LIBSTDCPP_USES_TBB)
+#  include <oneapi/tbb/global_control.h>
+#endif
+
 
 int main(int argc, char **argv)
 {
 #if defined(SANITIZER_ENABLED)
     QThreadPool::globalInstance()->setExpiryTimeout(0);
 #endif
-
+#if defined(BS_LIBSTDCPP_USES_TBB)
+    oneapi::tbb::task_scheduler_handle tbb { tbb::attach { } };
+#endif
     ApplicationType a(argc, argv);
     a.init();
     a.afterInit();
 
     int exitCode = a.exec();
 
-    // we are using QThreadStorage in thread-pool threads, so we have to make sure they are
+    // We are using QThreadStorage in thread-pool threads, so we have to make sure they are
     // all joined before destroying the static QThreadStorage objects.
+    // This goes for QThreadPool and also the libstdc++/libtbb thread-pool on Linux.
     QThreadPool::globalInstance()->clear();
     QThreadPool::globalInstance()->waitForDone();
+#if defined(BS_LIBSTDCPP_USES_TBB)
+    oneapi::tbb::finalize(tbb);
+#endif
 
     a.checkRestart();
     return exitCode;
