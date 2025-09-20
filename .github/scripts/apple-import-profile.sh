f5ffbe96ac4cbdd73366684e8e3e2379f6bf28c0
Robert Griebl <robert@griebl.org>
2025-09-20 13:11:01 +0200

    Github actions: fix iOS build

--- .github/scripts/apple-import-profile.sh
+++ .github/scripts/apple-import-profile.sh
@@ -2,5 +2,10 @@
 
 set -euo pipefail
 
+# iOS Distribution
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 echo "$PROVISIONING_PROFILE_DATA" | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/${PROVISIONING_PROFILE_SPECIFIER}.mobileprovision
+
+# Apple Distribution
+mkdir -p ~/Library/Developer/Xcode/UserData/Provisioning\ Profiles
+echo "$PROVISIONING_PROFILE_DATA" | base64 --decode > ~/Library/Developer/Xcode/UserData/Provisioning\ Profiles/${PROVISIONING_PROFILE_SPECIFIER}.mobileprovision
