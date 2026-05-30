71cb5b605dba868f9035c9ed2628c56a3ce32c31
Alexander Ritter <42546705+AlexanderRitter02@users.noreply.github.com>
2026-05-30 22:48:55 +0200

    Allow offline build for Flathub with FLATHUB_NETWORK_OFF option
    The FLATHUB_NETWORK_OFF option works _specifically_ *only* for Flatpak builds.
    
    It disables FetchContent used for QCoro, QAdwaitaDecorations & BuildSentry,
    and REQUIRES the sources for those to be in a specific "_deps" directory for the CMake build.
    These sources are provided in the Flatpak manifest for the build process.

--- cmake/BuildQAdwaitaDecorations.cmake
+++ cmake/BuildQAdwaitaDecorations.cmake
@@ -1,18 +1,23 @@
 # Copyright (C) 2004-2026 Robert Griebl
 # SPDX-License-Identifier: GPL-3.0-only
 
-include(FetchContent)
-
 set(USE_QT6 ON)
 
-FetchContent_Declare(
-    qadwaitadecorations
-    GIT_REPOSITORY https://github.com/FedoraQt/QAdwaitaDecorations.git
-    GIT_TAG        f40f31dadc074bd989db6dd90e52eecf33c4567b  # 0.1.5 + border fix
-    SOURCE_SUBDIR  "NeedManualAddSubDir" # make it possible to add_subdirectory below
-)
+if(FLATHUB_NETWORK_OFF)
+  set(qadwaitadecorations_SOURCE_DIR "${CMAKE_SOURCE_DIR}/_deps/qadwaitadecorations-src")
+  set(qadwaitadecorations_BINARY_DIR "${CMAKE_BINARY_DIR}/_deps/qadwaitadecorations-build")
+else()
+  include(FetchContent)
 
-FetchContent_MakeAvailable(qadwaitadecorations)
+  FetchContent_Declare(
+      qadwaitadecorations
+      GIT_REPOSITORY https://github.com/FedoraQt/QAdwaitaDecorations.git
+      GIT_TAG        f40f31dadc074bd989db6dd90e52eecf33c4567b  # 0.1.5 + border fix
+      SOURCE_SUBDIR  "NeedManualAddSubDir" # make it possible to add_subdirectory below
+  )
+
+  FetchContent_MakeAvailable(qadwaitadecorations)
+endif()
 
 set(mll ${CMAKE_MESSAGE_LOG_LEVEL})
 if (NOT VERBOSE_FETCH)
